7e6bae323b08efa0b8960536cf0f2c357f12e73d
YH <139380475+songjung-good@users.noreply.github.com>
2023-08-09 17:44:33 +0900

    [D1] Title: 최대 최소의 간격, Time: 147 ms, Memory: 58,524 KB -BaekjoonHub

---
+++ SWEA/D1/11092. 최대 최소의 간격/README.md
@@ -0,0 +1,11 @@
+# [D1] 최대 최소의 간격 - 11092 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXYEGnBq6h0DFAST) 
+
+### 성능 요약
+
+메모리: 58,524 KB, 시간: 147 ms, 코드길이: 454 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/11092. 최대 최소의 간격/최대 최소의 간격.py
@@ -0,0 +1,20 @@
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    min_idx = 0 #최솟값의 인덱스
+    max_idx = 0 #최대값의 인덱스
+    for i in range(1, N):
+        if arr[min_idx] > arr[i]:
+            min_idx = i
+        if arr[max_idx] <= arr[i]:
+            max_idx = i
+
+    m_ans = max_idx - min_idx
+    if m_ans < 0:
+        ans = m_ans * -1
+    else:
+        ans = m_ans
+        
+    print(f'#{tc} {ans}')
