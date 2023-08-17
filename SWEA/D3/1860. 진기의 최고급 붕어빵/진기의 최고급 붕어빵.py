80fbab0f7a2d3ff911132cfb3de2ed8658007516
YH <139380475+songjung-good@users.noreply.github.com>
2023-08-17 14:42:32 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 382 ms, Memory: 61,976 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -1,18 +1,54 @@
+'''
 T = int(input())
-for t in range(1, T+1):
+for tc in range(1, T+1):
+    #N 고객 수, M 빵만드는 시간, K 만드는 붕어빵 수
     N, M, K = map(int, input().split())
-    sec = sorted(list(map(int, input().split())))
-    res = 'Possible'
-    B = 0
-    cnt = 0
-    for i in range(N):
-        S = sec[i] // M
-        if S - cnt >= 1:
-            B += (S - cnt) * K
-            cnt += S - cnt
-        if B < 1 :
-            res = 'Impossible'
+    #고객이 오는 시간
+    customer_lst = list(map(int, input().split()))
+    customer_lst.sort()
+    time = 0
+    bread = 0
+    ans = 'Impossible'
+    # 0 ~ M 까지 손님오면 불가능
+    # M ~ 2M 까지 손님 K명 이상이면 불가능
+    # 조건 다 맞추고 N이 0이면 가능
+    while True:
+        time += M - 1
+        if len(customer_lst) == 0:
+            ans = 'Possible'
             break
+        if time % M == 0 and time != 0:
+            bread += K
+        if time == customer_lst[0]:
+            if bread > 0:
+                bread -= 1
+            else:
+                break
+            customer_lst.pop(0)
+        time += 1
+        if time % M == 0 and time != 0:
+            bread += K
+
+    print(f'#{tc} {ans}')
+'''
+T = int(input())
+for tc in range(1, T+1):
+    #N 고객 수, M 빵만드는 시간, K 만드는 붕어빵 수
+    N, M, K = map(int, input().split())
+    #고객이 오는 시간
+    customer_lst = list(map(int, input().split()))
+    customer_lst.sort()
+    time = 0
+    bread = 0
+    # 0 ~ M 까지 손님오면 불가능
+    # M ~ 2M 까지 손님 K명 이상이면 불가능
+    # 조건 다 맞추고 N이 0이면 가능
+    for i in range(N):
+        bread = customer_lst[i] // M * K - (i + 1)
+        if bread >= 0:
+            ans = 'Possible'
         else:
-            B -= 1
-    print(f'#{t} {res}')
+            ans = 'Impossible'
+            break
+
+    print(f'#{tc} {ans}')
