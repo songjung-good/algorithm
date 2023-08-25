a184f3061df7e961056cc73aad7903bc38e92b75
YH <139380475+songjung-good@users.noreply.github.com>
2023-08-25 14:19:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 1,556 ms, Memory: 64,000 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
@@ -1,16 +1,16 @@
 '''
 N*N 크기의 도시
 운영 비용 = K * K + (K - 1) * (K - 1)
-
+ 
 서비스 범위 만큼 마을 탐색
-
+ 
 1. 전체를 다 덮으려면 사이즈 어떻게 할까요?
  1) 전체 집 수, 전체 비용 먼저 탐색
 2. 하나씩 줄이면서 탐색
  1)
  abs(row - center_row) + abs(col - center_col) <= radius:
 '''
-
+ 
 #시작 좌표를 기준으로 주변의 집의 수 탐색
 def diamond_search(center_row, center_col, radius):
     house = 0
@@ -26,9 +26,9 @@ def diamond_search(center_row, center_col, radius):
                 if town[row][col] == 1:
                     house += 1
     return house
-
-
-
+ 
+ 
+ 
 T = int(input())
 for tc in range(1, T+1):
     #N 마을의 크기, M 가구 당 지불 가능 비용
@@ -52,20 +52,15 @@ for tc in range(1, T+1):
             #열
             for j in range(N):
                 #범위
-                for k in range(N):
+                for k in range(N, 0, -1):
                     #비용, 집의 수, 받는 돈
                     price = k * k + (k - 1) * (k - 1)
                     house_cnt = diamond_search(i, j, k)
                     income = house_cnt * M
                     #넓은 범위 부터 탐색이라 통과되면 바로 종료
-                    if price < income:
+                    if price <= income:
                         if ans < house_cnt:
                             ans = house_cnt
-                    elif price == income:
-                        if ans < house_cnt:
-                            ans = house_cnt
-
-                    else:
-                        pass
-                    
+                            break
+ 
     print(f'#{tc} {ans}')
