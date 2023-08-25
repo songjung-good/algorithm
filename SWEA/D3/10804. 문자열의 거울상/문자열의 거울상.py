a9512833b9f9b64a5def3739857f8c24a7611ad5
YH <139380475+songjung-good@users.noreply.github.com>
2023-08-25 19:58:30 +0900

    [D3] Title: 문자열의 거울상, Time: 149 ms, Memory: 59,600 KB -BaekjoonHub

--- SWEA/D3/10804. 문자열의 거울상/문자열의 거울상.py
+++ SWEA/D3/10804. 문자열의 거울상/문자열의 거울상.py
@@ -12,7 +12,7 @@
     [출력]
     각 테스트 케이스마다 주어진 문자열을 거울에 비춘 문자열로 출력한다.
 '''
-
+'''
 T = int(input())
 
 for tc in range(1, 1+T):
@@ -30,5 +30,48 @@ for tc in range(1, 1+T):
             new_str += 'p'
 
     ans = new_str[::-1]
+
+    print(f'#{tc} {ans}')
+'''
+'''
+    ‘b’, ‘d’, ‘p’, ‘q’로 이루어진 문자열이 주어진다.
+    이 문자열을 거울에 비추면 어떤 문자열이 되는지 구하는 프로그램을 작성하라.
+    예를 들어, “bdppq”를 거울에 비추면 “pqqbd”처럼 나타날 것이다.
+
+    [입력]
+    첫 번째 줄에 테스트 케이스의 수 T가 주어진다.
+    각 테스트 케이스의
+    첫 번째 줄에는 ‘b’, ‘d’, ‘p’, ‘q’만으로 이루어진 하나의 문자열이 주어진다.
+    문자열의 길이는 1이상 1000이하이다.
+
+    [출력]
+    각 테스트 케이스마다 주어진 문자열을 거울에 비춘 문자열로 출력한다.
+'''
+
+T = int(input())
+
+for tc in range(1, 1+T):
+    str_tc = input()
+    stack = []
+    top = -1
+    ans = ''
+
+    for i in str_tc:
+        if i == 'b':
+            stack += 'd'
+            top += 1
+        elif i == 'd':
+            stack += 'b'
+            top += 1
+        elif i == 'p':
+            stack += 'q'
+            top += 1
+        elif i == 'q':
+            stack += 'p'
+            top += 1
+
+    while top >= 0:
+        ans += stack[top]
+        top -= 1
 
     print(f'#{tc} {ans}')
