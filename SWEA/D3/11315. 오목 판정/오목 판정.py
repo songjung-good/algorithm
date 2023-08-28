16bd2e58f5520c0b701bb5a6921d300c245755f2
YH <139380475+songjung-good@users.noreply.github.com>
2023-08-28 09:04:04 +0900

    [D3] Title: 오목 판정, Time: 264 ms, Memory: 63,820 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,16 +1,9 @@
-T = int(input())
-for t in range(1, T+1):
-    N = int(input())
-    MAP = [[0]* N for _ in range(N)]
-    res = 'NO'
-    for i in range(N):
-        string = input()
-        for j in range(N):
-            if string[j] == 'o':
-                MAP[i][j] = 1
-
+def check():
+    global res
     for k in range(N):
         for l in range(N):
+            if res == 'YES':
+                return
             for dx, dy in [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]:
                 cnt = 0
                 for n in range(5):
@@ -20,6 +13,17 @@ for t in range(1, T+1):
                             cnt += 1
                 if cnt == 5:
                     res = 'YES'
-                    break
+                    return
 
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    MAP = [[0] * N for _ in range(N)]
+    res = 'NO'
+    for i in range(N):
+        string = input()
+        for j in range(N):
+            if string[j] == 'o':
+                MAP[i][j] = 1
+    check()
     print(f'#{t} {res}')
