1bc0e8eb7fd6d562ede808a578202177106dc72e
YH <139380475+songjung-good@users.noreply.github.com>
2023-08-28 09:23:27 +0900

    [D3] Title: 오목 판정, Time: 219 ms, Memory: 62,268 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,29 +1,28 @@
-def check():
-    global res
-    for k in range(N):
-        for l in range(N):
-            if res == 'YES':
-                return
-            for dx, dy in [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]:
-                cnt = 0
-                for n in range(5):
-                    nx, ny = k + (dx * n), l + (dy * n)
-                    if 0 <= nx < N and 0 <= ny < N:
-                        if MAP[nx][ny] == 1:
-                            cnt += 1
-                if cnt == 5:
-                    res = 'YES'
-                    return
-
 T = int(input())
-for t in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
-    MAP = [[0] * N for _ in range(N)]
-    res = 'NO'
+    board = [list(map(str, input())) for _ in range(N)]
+
+    di = [0, 1, 1, 1]
+    dj = [1, 1, 0, -1]
+    ans = 'NO'
+
     for i in range(N):
-        string = input()
         for j in range(N):
-            if string[j] == 'o':
-                MAP[i][j] = 1
-    check()
-    print(f'#{t} {res}')
+            if board[i][j] == 'o':
+                for k in range(4):
+                    cnt = 0
+                    for a in range(1, 5):
+                        ni = i + di[k] * a
+                        nj = j + dj[k] * a
+                        if 0 <= ni <N and 0 <= nj <N and board[ni][nj] == 'o':
+                            cnt += 1
+                        if cnt == 4:
+                            ans = 'YES'
+                            break
+                    if ans == 'YES':
+                        break
+        if ans == 'YES':
+            break
+
+    print(f'#{tc} {ans}')
