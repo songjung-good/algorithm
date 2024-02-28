5caba15201046b522174e2049206352a50b2031b
YH <139380475+songjung-good@users.noreply.github.com>
2024-02-28 09:18:36 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,10 @@
+def solution(a, b, c):
+    answer = 0
+    if a == b == c:
+        answer = (a+b+c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    elif a != b and b != c and a != c:
+        answer = a + b + c
+    else:
+        answer = (a+b+c) * (a**2 + b**2 + c**2)
+    
+    return answer
