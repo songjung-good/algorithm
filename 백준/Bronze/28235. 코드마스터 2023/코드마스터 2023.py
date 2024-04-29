460ab0c2a0830a00bacf60d648a7f8725aeb7431
YH <139380475+songjung-good@users.noreply.github.com>
2024-04-29 19:15:20 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+A = input()
+
+if A == 'SONGDO':
+    print("HIGHSCHOOL")
+elif A == 'CODE':
+    print('MASTER')
+elif A == '2023':
+    print('0611')
+elif A == 'ALGORITHM':
+    print('CONTEST')
