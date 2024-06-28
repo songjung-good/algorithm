11d65044514ccf1f7a63e527b73e56429cc2afcc
YH <139380475+songjung-good@users.noreply.github.com>
2024-06-28 22:22:20 +0900

    [Bronze III] Title: K 2K 게임, Time: 76 ms, Memory: 35108 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
@@ -1,14 +1,20 @@
 A, B = map(int, input().split())
 ans = []
 B = B % 10
-
+BB = (B*2) % 10
+X = 0
 for i in range(1, A+1):
     j = i
     if i >= 10:
         j = j % 10
-    if j == B or j == (2*B):
-        pass
-    else:
+    if j != B and j != BB:
         ans.append(i)
-print(len(ans))
-print(*ans)
+        X += 1
+    else:
+        pass
+
+if X == 0:
+    print(0)
+else:
+    print(X)
+    print(*ans)
