c5ef8da344832446c463eb1417e729c13ab80dbe
YH <139380475+songjung-good@users.noreply.github.com>
2024-07-20 21:11:42 +0900

    [Bronze I] Title: 비숍 투어, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.py
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.py
@@ -1,21 +1,23 @@
-def check(x, y, N, M):
-    if x < 1 or y < 1 or x > N or y > M:
-        return 1
-    else:
-        return 0
-
 N, M = map(int,input().split())
 sx, sy = map(int,input().split())
 ex, ey = map(int,input().split())
-S = (sx+sy) % 2
-E = (ex+ey) % 2
+SX = sx % 2
+SY = sy % 2
+EX = ex % 2
+EY = ey % 2
 
-A = check(sx, sy, N, M)
-B = check(ex, ey, N, M)
-
-if A == 1 or B == 1:
-    print('NO')
-elif S == E:
-    print('YES')
-elif S != E:
-    print('NO')
+if N == 1 or M == 1:
+    if sx == ex and sy == ey:
+        print('YES')
+    else:
+        print('NO')
+elif SX == EX:
+    if SY == EY:
+        print('YES')
+    else:
+        print('NO')
+elif SX != EX:
+    if SY != EY:
+        print('YES')
+    else:
+        print('NO')
