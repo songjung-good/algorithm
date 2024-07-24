92475f4f91003e151dcc801aa66be3e120abb4cc
YH <139380475+songjung-good@users.noreply.github.com>
2024-07-24 21:41:50 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,6 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+def CHECK(CNT, N, SIDE):
+    if CNT >= N:
+        return SIDE[N-1]
+    else:
+        TRI(CNT, N, SIDE)
+
+def TRI(CNT, N, SIDE):
+    for i in range(CNT, N):
+        new_num = SIDE[i-2] + SIDE[i-1]
+        SIDE.append(new_num)
+    return CHECK(N, N, SIDE)
+
 T = int(input())
 SIDE = [1, 1, 1]
 for t in range(T):
