e575b823032e87b4287a5fa9ce18fa6a57da751e
YH <139380475+songjung-good@users.noreply.github.com>
2024-10-12 20:08:07 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,14 @@
+CLASS = {
+    'Algorithm':	204,
+    'DataAnalysis':	207,
+    'ArtificialIntelligence':	302,
+    'CyberSecurity': 'B101',
+    'Network':	303,
+    'Startup':	501,
+    'TestStrategy':	105,
+}
+
+N = int(input())
+for n in range(N):
+    A = input()
+    print(CLASS[A])
