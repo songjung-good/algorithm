5f63c844c27669a4e7e56ed2dee77727d316e749
YH <139380475+songjung-good@users.noreply.github.com>
2024-10-25 21:55:12 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 364 ms, Memory: 71112 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -5,7 +5,7 @@ if N <= 2:
     num2 = fibo_lst[N]
 else:
     for n in range(3, N+1):
-        fibo_num = fibo_lst[n-1]%1000000007 + fibo_lst[n-2]%1000000007
+        fibo_num = (fibo_lst[n-1] + fibo_lst[n-2])%1000000007
         fibo_lst.append(fibo_num)
     num2 = fibo_lst[N]
 
