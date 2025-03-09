610d22d957233888dca139c8517f8691d0b754c6
YH <139380475+songjung-good@users.noreply.github.com>
2025-03-10 01:20:30 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 40 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,17 +1,14 @@
+from bisect import bisect_left
+
 N = int(input())
 N_lst = list(map(int, input().split()))
-len_lst = [0] * N
-len_lst[N-1] = 1
+dp = []
 
-for n in range(N-2,-1,-1):
-    now = N_lst[n]
-    cnt = 0
-    for m in range(n+1, N):
-        if now >= N_lst[m]:
-            pass
-        else:
-            if len_lst[m] > cnt:
-                cnt = len_lst[m]
-    len_lst[n] = cnt + 1
+for num in N_lst:
+    idx = bisect_left(dp, num)  # num이 들어갈 위치 찾기
+    if idx == len(dp):
+        dp.append(num)  # 가장 큰 값이면 추가
+    else:
+        dp[idx] = num  # 적절한 위치에 갱신 (길이는 유지)
 
-print(max(len_lst))
+print(len(dp))
