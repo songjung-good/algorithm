a2629c69ac3492c9299bdb2cdaa9ae64a9587fec
YH <139380475+songjung-good@users.noreply.github.com>
2025-04-02 13:33:46 +0900

    [Silver V] Title: 미아 노트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20114. 미아 노트/미아 노트.py
+++ 백준/Silver/20114. 미아 노트/미아 노트.py
@@ -2,20 +2,17 @@ N, H, W = map(int, input().split())
 MAP = [list(input()) for _ in range(H)]
 ans = ''
 for n in range(N):
-    word = ''
+    found = False
     for h in range(H):
+        if found:
+            break
         for w in range(W):
             now = MAP[h][W*n+w]
-            if now == '?':
-                pass
-            else:
-                word = now
+            if now != '?':
+                ans += now
+                found = True
                 break
-        if word != '':
-            break
-    if word == '':
+    if not found:
         ans += '?'
-    else:
-        ans += word
 
 print(ans)
