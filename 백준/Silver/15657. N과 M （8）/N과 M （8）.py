3ec7155532199613e2ee2bb5f75fb9f9dc614530
YH <139380475+songjung-good@users.noreply.github.com>
2025-04-04 01:32:59 +0900

    [Silver III] Title: N과 M (8), Time: 136 ms, Memory: 111436 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,17 +1,14 @@
-def dfs(n):
-    if len(s) == M:
-        print(*s)
-        return
-
-    for i in range(n, N):
-        x = lst[i]
-        s.append(x)
-        dfs(i)
-        s.pop()
-
-
 N, M = map(int, input().split())
-lst = list(map(int, input().split()))
-lst.sort()
-s = []
-dfs(0)
+N_lst = list(map(int, input().split()))
+N_lst.sort()
+lst = [0] * M
+
+def num(lst, n, m):
+    if m == M:
+        print(*lst)
+    else:
+        for i in range(n, N):
+            lst[m] = N_lst[i]
+            num(lst, i, m+1)
+
+num(lst, 0, 0)
