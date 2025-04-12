d60a18df625807b763dca9e7a4da660136996890
YH <139380475+songjung-good@users.noreply.github.com>
2025-04-12 17:55:30 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,12 @@
+def solution(n, slicer, num_list):
+    answer = []
+    a,b,c = slicer
+    if n == 1:
+        answer = num_list[:b+1]
+    elif n == 2:
+        answer = num_list[a:]
+    elif n == 3:
+        answer = num_list[a:b+1]
+    else:
+        answer = num_list[a:b+1:c]
+    return answer
