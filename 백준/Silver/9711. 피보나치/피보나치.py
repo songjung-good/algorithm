0845b4476925d01d33b4a4b04fcd9416fb402b43
YH <139380475+songjung-good@users.noreply.github.com>
2025-06-03 20:52:11 +0900

    [Silver III] Title: 피보나치, Time: 620 ms, Memory: 36892 KB -BaekjoonHub

--- 백준/Silver/9711. 피보나치/피보나치.py
+++ 백준/Silver/9711. 피보나치/피보나치.py
@@ -1,15 +1,17 @@
-fibo_lst = [1, 1]
+dp = [0] * 10001
 fibo_len = 2
+dp[0] = 1
+dp[1] = 1
 
 def fibo(num):
-    global fibo_len, fibo_lst
+    global fibo_len, dp
     if fibo_len >= num:
-        return fibo_lst[num-1]
+        return dp[num-1]
     else:
-        for i in range(fibo_len-1, num-1):
-            fibo_lst.append(fibo_lst[i-1] + fibo_lst[i])
+        for i in range(fibo_len, num):
+            dp[i] = dp[i-2] + dp[i-1]
         fibo_len = num
-        return fibo_lst[num-1]
+        return dp[num-1]
 
 T = int(input())
 for t in range(1, T+1):
