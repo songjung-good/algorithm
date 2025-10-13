3e7c2d08f9e909bf24542bbd33b02813b4b34b10
YH <139380475+songjung-good@users.noreply.github.com>
2025-10-14 02:46:52 +0900

    [Silver II] Title: N과 M (9), Time: 100 ms, Memory: 37528 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,21 +1,24 @@
-def dfs(v):
-    if len(s) == M:
-        print(*s)
-        return
-
-    used = {}  # 중복 검사용 딕셔너리
-    for i in range(N): # {dic}.get(key, default) 키가 있으면 벨류값을 반환, 없으면 디폴트를 벨류로 하는 새 딕셔너리 추가
-        if i not in v and used.get(lst[i], 0) == 0:
-            used[lst[i]] = 1
-            s.append(lst[i])
-            new_v = v + [i]
-            dfs(new_v)
-            s.pop()
-
+from itertools import combinations, permutations
 
 N, M = map(int, input().split())
-lst = list(map(int, input().split()))
-lst.sort()
-s = []
-v = []
-dfs(v)
+N_lst = list(map(int, input().split()))
+
+if M == 1:
+    past = 0
+    N_lst.sort()
+    for i in N_lst:
+        if i == past:
+            pass
+        else:
+            print(i)
+        past = i
+else:
+    comb = list(permutations(N_lst, M))
+    comb.sort()
+    past = ()
+    for i in comb:
+        if i == past:
+            pass
+        else:
+            print(*i)
+        past = i
