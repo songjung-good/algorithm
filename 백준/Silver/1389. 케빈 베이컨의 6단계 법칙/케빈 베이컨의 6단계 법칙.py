35c087bac3a895dc737887e9bf496b6b2896a827
YH <139380475+songjung-good@users.noreply.github.com>
2025-10-30 17:56:51 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,32 +1,35 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
+def BFS(start):
+    q = deque([start])
+    visited = [-1] * N
+    visited[start] = 0
+
+    while q:
+        now = q.popleft()
+        for next in GRAPH[now]:
+            if visited[next] == -1:
+                visited[next] = visited[now] + 1
+                q.append(next)
+
+    return sum(visited)
+
 N, M = map(int,input().split())
-INF = 1e9
-DP = [[INF] * N for _ in range(N)]
+GRAPH = [[] for _ in range(N)]
 
-# 자신과의 거리 0
-for n in range(N):
-    DP[n][n] = 0
-
-# 직접 닿은 거리 1
 for _ in range(M):
-    n, m = map(int,input().split())
-    DP[n-1][m-1] = 1
-    DP[m-1][n-1] = 1
+    n, m = map(int, input().split())
+    GRAPH[n-1].append(m-1)
+    GRAPH[m-1].append(n-1)
 
-# 나머지
-for i in range(N):
-    for j in range(N):
-        for k in range(N):
-            DP[j][k] = min(DP[j][k], DP[j][i] + DP[i][k])
-
-min_gap = INF
+min_gap = 1e9
 ans = 0
 for n in range(N):
-    cnt = sum(DP[n])
+    cnt = BFS(n)
     if min_gap > cnt:
-       min_gap = cnt
-       ans = n
+        min_gap = cnt
+        ans = n+1
 
-print(ans+1)
+print(ans)
