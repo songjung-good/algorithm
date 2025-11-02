eea94d4e766c02caf0b0ff10d2ec8a313035ce8a
YH <139380475+songjung-good@users.noreply.github.com>
2025-11-02 13:55:54 +0900

    [Silver II] Title: N과 M (12), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,22 +1,16 @@
-def dfs(n):
-
-    if len(s) == M:
-        s.sort()
-        new_s = tuple(s)
-        if check.get(new_s, 0) == 0:
-            check[new_s] = 1
-            print(*s)
-        return
-
-    for i in range(n, N):
-        s.append(lst[i])
-        dfs(i)
-        s.pop()
+# 코드를 작성해주세요
+def combi(n, cnt, lst):
+    for i in range(cnt, num):
+        lst[n] = N_lst[i]
+        if n == M-1:
+            print(*lst)
+        else:
+            combi(n+1, i, lst)
+    return 
 
 
 N, M = map(int, input().split())
-lst = list(map(int, input().split()))
-lst.sort()
-s = []
-check = {}
-dfs(0)
+N_lst = sorted(list(set(map(int,input().split()))))
+num = len(N_lst)
+ans = [0] * M
+combi(0, 0, ans)
