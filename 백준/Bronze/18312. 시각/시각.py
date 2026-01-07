547bc7d36c1420dd9caa80988013f46118a19c03
YH <139380475+songjung-good@users.noreply.github.com>
2026-01-07 19:58:23 +0900

    [Bronze II] Title: 시각, Time: 108 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Bronze/18312. 시각/시각.py
+++ 백준/Bronze/18312. 시각/시각.py
@@ -1,13 +1,10 @@
 N,K=input().split()
-cnt=0
+X=0
 for h in range(int(N)+1):
-    H=str(h)
     for m in range(60):
-        M=str(m)
         for s in range(60):
-            S=str(s)
-            ans=H+M+S
-            if len(ans)<6:ans+='0'
-            if K in ans:
-                cnt+=1
-print(cnt)
+            Y=str(h)+str(m)+str(s)
+            if len(Y)<6:Y+='0'
+            if K in Y:
+                X+=1
+print(X)
