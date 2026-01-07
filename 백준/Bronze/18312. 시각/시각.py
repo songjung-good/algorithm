c1b55d7f0622de335f27027ad9ce17196cfaad3f
YH <139380475+songjung-good@users.noreply.github.com>
2026-01-07 19:56:13 +0900

    [Bronze II] Title: 시각, Time: 108 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Bronze/18312. 시각/시각.py
+++ 백준/Bronze/18312. 시각/시각.py
@@ -1,26 +1,13 @@
-'''
-이코테 구현파트
-예제 4-2
-'''
-
-N, K = map(str, input().split())
-cnt = 0
+N,K=input().split()
+cnt=0
 for h in range(int(N)+1):
-    if h < 10:
-        H = '0' + str(h)
-    else:
-        H = str(h)
+    H=str(h)
     for m in range(60):
-        if m < 10:
-            M = '0' + str(m)
-        else:
-            M = str(m)
+        M=str(m)
         for s in range(60):
-            if s < 10:
-                S = '0' + str(s)
-            else:
-                S = str(s)
-            if K in H + M + S:
-                cnt += 1
-
+            S=str(s)
+            ans=H+M+S
+            if len(ans)<6:ans+='0'
+            if K in ans:
+                cnt+=1
 print(cnt)
