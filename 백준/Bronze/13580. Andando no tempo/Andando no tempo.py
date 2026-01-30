3dee99c3e4a65a746d573afa22089176ccc9c361
YH <139380475+songjung-good@users.noreply.github.com>
2026-01-31 00:43:33 +0900

    [Bronze IV] Title: Andando no tempo, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.py
@@ -0,0 +1,6 @@
+lst = list(map(int, input().split()))
+lst.sort()
+if lst[0] == lst[1] or lst[1] == lst[2] or lst[0]+lst[1] == lst[2] or lst[0] == lst[1]+lst[2]:
+    print('S')
+else:
+    print('N')
