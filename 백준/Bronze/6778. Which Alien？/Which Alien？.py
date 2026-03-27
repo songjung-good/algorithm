7e39759a34490e0ec6d060c99898fcff401ecf58
YH <139380475+songjung-good@users.noreply.github.com>
2026-03-28 03:17:07 +0900

    [Bronze IV] Title: Which Alien?, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,10 @@
+# 코드를 작성해주세요
+N=int(input())
+M =int(input())
+
+if N >= 3 and M <= 4:
+    print('TroyMartian')
+if N <= 6 and M >=2:
+    print('VladSaturnian')
+if N <= 2 and M <=3:
+    print('GraemeMercurian')
