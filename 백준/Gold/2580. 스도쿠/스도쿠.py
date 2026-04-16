32ada8cf57c8b0e107d0b02e58003cb77a0ed369
YH <139380475+songjung-good@users.noreply.github.com>
2026-04-16 17:31:17 +0900

    [Gold IV] Title: 스도쿠, Time: 2464 ms, Memory: 129176 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,49 @@
+def check_sq(x, y, i):
+    min_r, max_r = x//3*3, x//3 * 3 + 3
+    min_c, max_c = y//3*3, y//3 * 3 + 3
+    lst = []
+    for r in range(min_r, max_r):
+        for c in range(min_c, max_c):
+            lst.append(board[r][c])
+            
+    if i not in lst:
+        return True
+    else:
+        return False
+    
+def check_col(y, i):
+    if i not in [board[r][y] for r in range(9)]:
+        return True
+    else:
+        return False
+        
+def check_row(x, i):
+    if i not in board[x]:
+        return True
+    else:
+        return False
+
+def find_num(lst):
+    for x, y in lst:
+        if board[x][y]:
+            pass
+        else:
+            for i in range(1, 10):
+                if check_row(x, i) and check_col(y, i) and check_sq(x, y, i):
+                    board[x][y] = i
+                    if find_num(lst):
+                        return True
+                    board[x][y] = 0
+            return False
+    return True
+
+board = [list(map(int, input().split())) for _ in range(9)]
+blank = []
+for i in range(9):
+    for j in range(9):
+        if board[i][j] == 0:
+            blank.append((i,j))
+            
+find_num(blank)
+for r in board:
+    print(*r)
